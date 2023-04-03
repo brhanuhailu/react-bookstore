99fa618d92672ac9499bbc33e785a09eebdc1194
brhanuhailu <hailubrhanu29@gmail.com>
2023-04-03 07:13:55 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Books from './components/books';
 import './App.css';
 import Categories from './components/categories';
@@ -9,12 +9,14 @@ class App extends PureComponent {
   render() {
     return (
       <div>
-        <Routes>
-          <Route path="/" element={<Links />}>
-            <Route index element={<Books />} />
-            <Route path="categories" element={<Categories />} />
-          </Route>
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Links />}>
+              <Route index element={<Books />} />
+              <Route path="categories" element={<Categories />} />
+            </Route>
+          </Routes>
+        </Router>
       </div>
     );
   }
