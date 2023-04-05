63356b4488561043e47936704fdeec74799c34ef
brhanuhailu <hailubrhanu29@gmail.com>
2023-04-05 06:17:41 -0700

    categories.js

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import '../App.css';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { checkStatusAction } from '../redux/categories/categoriesSlice';
 
 const Categories = () => {
+  const categories = useSelector((state) => state.categories);
   const dispatch = useDispatch();
   return (
-    <div className="categories">
+    <div>
       <h1>Categories</h1>
       <button type="button" onClick={() => dispatch(checkStatusAction())}>
         Check Status
       </button>
+      <p>{categories}</p>
     </div>
   );
 };
